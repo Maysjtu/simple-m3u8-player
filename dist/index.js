f49117c55cc91dbc3d6025f296c2e62aad5b690b
Mayde <maysjtu@163.com>
2018-07-18 17:46:36 +0800

    github pages

--- dist/index.js
+++ dist/index.js
@@ -12,8 +12,8 @@ var changeBtn = document.getElementById('change');
 
 button.addEventListener('click', function () {
 	myPlayer.loadSource({
-		rendition0: '../docs/assets/cg.m3u8',
-		rendition1: '../docs/assets/cg_160.m3u8'
+		rendition0: 'http://p1yseh5av.bkt.clouddn.com/media/cg.m3u8',
+		rendition1: 'http://p1yseh5av.bkt.clouddn.com/media/cg_160.m3u8'
 	});
 });
 
